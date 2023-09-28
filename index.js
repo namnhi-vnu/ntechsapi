01f943874f09059e8c0e2de1d66c5eb0e3a5733c
namnhi-vnu <namnhi.vnu@gmail.com>
2023-09-28 22:25:07 +0700

    update query

--- index.js
+++ index.js
@@ -8,22 +8,29 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
-const apiUrl = process.env.API_URL;
-const base64Credentials = Buffer.from(`${username}:${password}`).toString(
-    "base64"
-);
+const username = "nt.vanha@1997";
+const password = "Ntechs@2022!";
+const perPage = 20; // Số bài viết trên mỗi trang
+const apiUrl = "https://ntechs.vn/wp-json/wp/v2/posts"; // URL API cơ bản
+
 app.get("/get-posts", async (req, res) => {
+    // Lấy tham số "page" từ query string (mặc định là trang 1 nếu không có)
+    const page = req.query.page || 1;
+
     const axiosInstance = axios.create({
         baseURL: apiUrl,
         headers: {
-            Authorization: `Basic ${base64Credentials}`,
+            Authorization: `Basic ${Buffer.from(
+                `${username}:${password}`
+            ).toString("base64")}`,
         },
     });
 
+    // Thêm tham số "page" vào URL để lấy trang tương ứng
+    const urlWithPage = `${apiUrl}?per_page=${perPage}&page=${page}`;
+
     axiosInstance
-        .get("/")
+        .get(urlWithPage)
         .then((response) => {
             const posts = response.data;
             res.json(posts);
